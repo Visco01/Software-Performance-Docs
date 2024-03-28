06096bfd4abbde8402cf3e1d6d43457a21edb249
Visco01 <visconti373@gmail.com>
2024-03-28 10:26:17 +0100

    Add processor sharing analysis

--- code/assignment2.py
+++ code/assignment2.py
@@ -10,10 +10,19 @@ def rho(x, l, alfa, maxv):
     return integral[0]*l
 
 fig, ax = plt.subplots(1, 1)
+max_y_value = 15
 alfa = 1.4
 maxv = 10000
-x = np.linspace(1,20,100)
-ax.plot(x, truncpareto.pdf(x, alfa, maxv), 'r-', lw=2, alpha=0.6, label='truncpareto pdf')
+rho = 0.7
+mu = truncpareto.mean(alfa, maxv)
+l = rho * mu
+
+# ax.set_ylim(0, max_y_value)
+
+x_values = np.linspace(1,20,100)
+
+
+ax.plot(x_values, truncpareto.pdf(x_values, alfa, maxv), 'r-', lw=2, alpha=0.6, label='truncpareto pdf')
 plt.show()
 
 # mu = 3.14... and rho = 0.7
